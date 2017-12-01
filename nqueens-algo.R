4bdb384fa53d901f012ec8d224e65b9626e4f2f4
Alejandro Jiménez <aljrico@gmail.com>
2017-12-02 00:36:57 +0100

    completed algo

--- nqueens-algo.R
+++ nqueens-algo.R
@@ -9,6 +9,7 @@
 # Libraries ---------------------------------------------------------------
 
 library(reshape)
+library(ggplot2)
 library(viridis)
 
 
@@ -91,6 +92,148 @@ tournament <- function(nproblem,noff,pmut,mort){
 }
 
 
+# Kill Half Model ---------------------------------------------------------
+half <- function(nproblem,noff,pmut,mort){
+
+	# List to be filled
+	offspring <- list()
+	population <- list()
+
+	# Arrays to be filled
+	son <- c()
+	subject <- c()
+	fitness <- c()
+	av.fitness <- c()
+	max.fitness <- c()
+
+	# Count Variables
+	b <- 0
+	ind <- 0
+
+	# Initial Population
+	ipop <- (nproblem)^2*2
+	for(i in 1:ipop){
+		population[[i]] <- sample(seq(1:nproblem))
+	}
+
+	# Reproduction
+
+	repeat{
+		m <- 1
+		ind <- ind + length(population)
+		while(length(population)>1){
+			# Selecting parents from population
+			par1 <- population[[1]]; population <- population[-1]
+			par2 <- population[[1]]; population <- population[-1]
+
+			# Measuring fitness of every child
+			for (i in 1:noff){
+				offspring[[m]] <- gen.offspring(par1,par2,noff,pmut)[[i]]
+				subject <- offspring[[m]]
+				fitness[m] <- (1/(1+meas.error(subject)))
+				m <- m+1
+			}
+		}
+
+		# Performance measures
+		av.fitness <- append(av.fitness, mean(fitness))
+		max.fitness <- append(max.fitness, max(fitness))
+
+		# Get the most fittest individual
+		bestguy <- offspring[max(fitness,index.return=TRUE)]
+		if (max(fitness) == 1) {
+			print <- 'SUCCESS!'
+			break
+		}
+
+		# Kill all parents
+		population <- list()
+		rm(par1,par2)
+
+		# Populating the new world
+		a <- round(length(offspring)*(1-mort)*0.5)*2
+		k <- 1
+		while (length(offspring) != 0){
+			population[[k]] <- offspring[[max(fitness,index.return=TRUE)]]
+			fitness <- fitness[-max(fitness,index.return=TRUE)]
+			offspring <- offspring[-max(fitness,index.return=TRUE)]
+			k <- k +1
+			if(k > a) break
+		}
+		b <- b+1
+		offspring <- list() # Kill all children not fitted enough
+
+		if (b > 5000) {
+			print <- "FAILURE"
+			break
+		}
+		if (length(population)<2){
+			print <- 'EXTINCTION'
+			break
+		}
+	}
+	return("ind" = ind)
+}
+
+
+# Kill all but two Model --------------------------------------------------
+allbut2 <- function(nproblem,noff,pmut){
+	# Load functions
+	source("genetic_functions.R")
+
+	# Seed for generating initial parents
+	seed <- seq(nproblem)
+
+	# List to be filled
+	offspring <- list()
+
+	# Arrays to be filled
+	son <- c()
+	subject <- c()
+	fitness <- c()
+	max.fitness <- c()
+
+	# Number of toddlers by couple
+	noff <- nproblem*5
+
+	# Initial parents
+	par1 <- sample(seed)
+	par2 <- sample(seed)
+
+	# Count variables
+	b <- 0
+	ind <- 0
+
+	# Creating further population
+	repeat{
+		# Measuring fitness of every child
+		for (i in 1:noff){
+			offspring[[i]] <- gen.offspring(par1,par2,noff, pmut)[[i]]
+			subject <- offspring[[i]]
+			fitness[i] <- (1/(1+meas.error(subject)))
+		}
+
+		ind <- ind + length(offspring)
+
+		# Selecting most fittest childs as new parents
+		par1 <- offspring[sort(-fitness,index.return=TRUE)[[2]][1]][[1]]
+		par2 <- offspring[sort(-fitness,index.return=TRUE)[[2]][2]][[1]]
+		max.fitness <- append(max.fitness, max(fitness))
+
+		b <- b+1
+
+		if (max(fitness)== nproblem*(nproblem-1)) break
+		if (b > 5000) break
+		if (max(fitness) == 1) break
+	}
+	return("ind" = ind)
+}
+
+
+
+
+# Evaluation --------------------------------------------------------------
+
 nproblem <- 8
 noff <- 5
 pmut <- 0.01
@@ -112,30 +255,107 @@ results <- list()
 ind <- c()
 nqueens <- c()
 mutate <- c()
-ite <- 100
+ite <- 50
 data.list <- list()
 pmuts <- c(0.01,0.05,0.1,0.25,0.5,0.75,0.9,1)
+lnq <- c(6,8)
 
 
-for(j in 1:length(pmuts)){
-	for(i in 1:ite){
-		pmut <- pmuts[j]
-		ind [i] <- tournament(nproblem,noff,pmut,mort)
-		nqueens[i] <- nproblem
-		mutate[i] <- pmut
-		mort[i] <- mort
-		print(i*100/ite)
+
+# Evaluation of Tournament Model ------------------------------------------
+
+for(k in 1:length(lnq))	{
+	nproblem <- lnq[k]
+	for(j in 1:length(pmuts)){
+		for(i in 1:ite){
+			pmut <- pmuts[j]
+			ind [i] <- tournament(nproblem,noff,pmut,mort)
+			nqueens[i] <- nproblem
+			mutate[i] <- pmut
+			print(i,j,k)
+		}
+		d1 <- data.frame(ind,nqueens,mutate)
+		data.list[[j + length(pmuts)*(k-1)]] <- d1
 	}
-	d1 <- data.frame(ind,nqueens,mutate,mort)
-	data.list[[j]] <- d1
 }
 
-dat.mut <- do.call(rbind, data.list)
+dat.tourn <- do.call(rbind, data.list)
 
-ggplot(dat.mut, aes(x=as.factor(mutate), y = ind, fill = as.factor(mutate))) +
-	geom_violin(bw=1000) +
+ggplot(dat.tourn, aes(x=as.factor(mutate), y = ind, fill = as.factor(mutate))) +
+	geom_violin(bw=1000, scale="area") +
 	stat_summary(fun.y=mean, geom="point", size=2) +
 	scale_fill_viridis(discrete=TRUE)
 
+ggplot(dat.tourn, aes(x=as.factor(nqueens), y = ind, colour = as.factor(mutate))) +
+	geom_jitter() +
+	scale_colour_viridis(discrete=TRUE)
+
+
+# Evaluation of Half Model ------------------------------------------------
+
+for(k in 1:length(lnq))	{
+	nproblem <- lnq[k]
+	for(j in 1:length(pmuts)){
+		for(i in 1:ite){
+			pmut <- pmuts[j]
+			ind [i] <- half(nproblem,noff,pmut,mort)
+			nqueens[i] <- nproblem
+			mutate[i] <- pmut
+			print(i,j,k)
+		}
+		d1 <- data.frame(ind,nqueens,mutate)
+		data.list[[j + length(pmuts)*(k-1)]] <- d1
+	}
+}
+
+dat.half <- do.call(rbind, data.list)
+
+ggplot(dat.half, aes(x=as.factor(mutate), y = ind, fill = as.factor(mutate))) +
+	geom_violin(bw=8, scale="area") +
+	stat_summary(fun.y=mean, geom="point", size=2) +
+	scale_fill_viridis(discrete=TRUE)
+
+ggplot(dat.half, aes(x=as.factor(nqueens), y = ind, colour = as.factor(mutate))) +
+	geom_jitter() +
+	scale_colour_viridis(discrete=TRUE)
+
+# Evaluation of Kill All But Two Model ------------------------------------------------
+
+for(k in 1:length(lnq))	{
+	nproblem <- lnq[k]
+	for(j in 1:length(pmuts)){
+		for(i in 1:ite){
+			pmut <- pmuts[j]
+			ind[i] <- allbut2(nproblem,noff,pmut)
+			nqueens[i] <- nproblem
+			mutate[i] <- pmut
+			print(i)
+			print(j)
+			print(k)
+		}
+		d1 <- data.frame(ind,nqueens,mutate)
+		data.list[[j + length(pmuts)*(k-1)]] <- d1
+	}
+}
+
+dat.allbut2 <- do.call(rbind, data.list)
+
+# Save data
+
+write.csv(dat.allbut2, file = "allbut2.csv")
+write.csv(dat.half, file = "half.csv")
+write.csv(dat.tourn, file = "tourn.csv")
+
+
+
+
+ggplot(dat.allbut2, aes(x=as.factor(mutate), y = ind, fill = as.factor(mutate))) +
+	geom_violin(bw=800, scale="area") +
+	stat_summary(fun.y=mean, geom="point", size=2) +
+	scale_fill_viridis(discrete=TRUE)
+
+ggplot(dat.allbut2, aes(x=as.factor(nqueens), y = ind, colour = as.factor(mutate))) +
+	geom_jitter() +
+	scale_colour_viridis(discrete=TRUE)
 
 
