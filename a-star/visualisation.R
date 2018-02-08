256918846c71aae6f2102ab5e69fb68f72695c99
Alejandro Jiménez <aljrico@gmail.com>
2018-02-08 16:15:21 +0100

    better visualisation

--- a-star/visualisation.R
+++ a-star/visualisation.R
@@ -9,7 +9,6 @@ library(ggmap)
 library(ggplot2)
 library(mapproj)
 
-
 # Load data ---------------------------------------------------------------
 
 d <- as.data.frame(fread("data/path.csv"))
@@ -18,10 +17,13 @@ d <- as.data.frame(fread("data/path.csv"))
 
 # Map ---------------------------------------------------------------------
 
-
 map <- get_map(location = c(lon = mean(-1), lat = mean(40)), zoom = 6,
 							 maptype = "roadmap", source = "google")
 p <- ggmap(map) +
 	scale_x_continuous(limits=c(-6,2.5), expand = c(0.1,0)) +
 	scale_y_continuous(limits=c(36,42), expand = c(0,0))
-p + geom_path(data=d, aes(x=longitude, y=latitude), color="black", size=1)
+p + geom_path(data=d, aes(x=longitude, y=latitude), colour="black", size=0.75) +
+	geom_point(aes(x = d[1,2], y=d[1,1]), colour = "dark blue", size = 5) +
+	geom_point(aes(x = d[1,2], y=d[1,1]), colour = "blue", size = 2) +
+	geom_point(aes(x = d[length(d$latitude),2], y=d[length(d$latitude),1]), colour = "dark red", size = 5) +
+	geom_point(aes(x = d[length(d$latitude),2], y=d[length(d$latitude),1]), colour = "red", size = 2)
